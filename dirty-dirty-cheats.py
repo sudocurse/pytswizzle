a42e7931b1f2ed362b31e4c6bb4598db8f1c7294
Ankeet Presswala <apresswala@linkedin.com>
2018-07-25 17:14:37 -0700

    because of course i had to make it a six element array for an eight line chorus

--- dirty-dirty-cheats.py
+++ dirty-dirty-cheats.py
@@ -3,11 +3,7 @@ prepositions = ['cause', 'and', '', '', 'and', '']
 nouns = ['players', 'haters', 'baby, I\'m just gonna', 'heartbreakers', 'fakers', 'baby I\'m just gonna']
 verbs = ['play', 'hate', 'shake', 'break', 'fake', 'shake']
 
-cursor = 0 
-
-while cursor < len(verbs):
-    if (cursor + 1) % 4 is 0:
-        print "{}, {}".format(title, title)
+for cursor in range(0, len(verbs)):
     output = [prepositions[cursor]]
     if prepositions[cursor] is not '':
         output.append(' the ')
@@ -15,5 +11,7 @@ while cursor < len(verbs):
     v = verbs[cursor]
     output.append('{}, {}, {}, {}, {}'.format(v, v, v, v, v))
     print ''.join(output)
+    if v is 'shake':
+        print "{}, {}".format(title, title)
     cursor += 1
 
