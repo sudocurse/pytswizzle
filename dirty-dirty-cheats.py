8cb9f2274746557e535004508431dc2693d72cc9
Ankeet Presswala <apresswala@linkedin.com>
2018-07-25 17:02:46 -0700

    what happens when you code while transferring between trains

--- dirty-dirty-cheats.py
+++ dirty-dirty-cheats.py
@@ -6,6 +6,8 @@ verbs = ['play', 'hate', 'shake', 'break', 'fake', 'shake']
 cursor = 0 
 
 while cursor < len(verbs):
+    if (cursor + 1) % 4 is 0:
+        print "{}, {}".format(title, title)
     output = [prepositions[cursor]]
     if prepositions[cursor] is not '':
         output.append(' the ')
@@ -13,7 +15,5 @@ while cursor < len(verbs):
     v = verbs[cursor]
     output.append('{}, {}, {}, {}, {}'.format(v, v, v, v, v))
     print ''.join(output)
-    if (cursor + 1) % 4 is 0:
-        print "{}, {}".format(title, title)
     cursor += 1
 
